a57e08b91963a362f08f97c87c1c850266532f53
Daniel <danieldelacruzprieto@gmail.com>
2020-12-23 18:32:08 -0500

    Haciendo las demostraciones combinatorias y de teoria de numeros del ejercicio D2TheWall y ponendo una posible respuesta en el codigo .py del ejercicio (todavia no la he aceptado en el COJ) pero debe de estar bien

--- D2TheWall/code/D2TheWall.py
+++ D2TheWall/code/D2TheWall.py
@@ -1,21 +1,23 @@
 import sys 
-import math
 
-MOD = 10**6+3
-n=2
+MOD = 10**6+3  # el modulo que es un numero primo 
+fact = [1] * (7* 10**5 + 10)  #un array de tamano  7* 10**5 + 10 para guaradar los valores del factorial precalculado 
 
-fact = [1] * (5* 10**5 + 10) 
-
+# para calcular el factorial modulo MOD hasta la cuota superior que es 5*10^5 + 2 * 10^5 
 for i in range (1,len(fact)):
     fact[i] = (fact[i-1] * i) % MOD
 
+# Calcula el Coeficiente binomico modulo MOD 
 def C(n,k):
-    return fact[n] * (pow (fact[k] , MOD -2,MOD)**2) % MOD 
+    return fact[n] * (pow (fact[k] , MOD -2,MOD)*pow (fact[n] , MOD -2,MOD)) % MOD   
 
-def isprime(n) :
-    if n < 4:
-        return True 
-    for i in range(2,int(math.sqrt(n))+1):
-        if n%i == 0 :
-            return False
-    return True
+def solution(n,C):
+    return (C(n+C,C)) -1  # calcular el coeficiente binomico n+c en c y restarle el caso en el que el muro no tiene bloques , que es la s
+
+# def isprime(n) :
+#     if n < 4:
+#         return True 
+#     for i in range(2,int(math.sqrt(n))+1):
+#         if n%i == 0 :
+#             return False
+#     return True
